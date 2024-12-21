69065123781f9bfb26426291f9e1b3b369ec57c1
DeepakSaravanan98 <sdeepakvlr@gmail.com>
2024-12-21 15:15:58 +0530

    newurl

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,3 +1,3 @@
 //export const baseUrl = "http://localhost:5000"; /*This base url will only change after*/
  //we deploy our app so we are taking that in a separate variable */ 
-export const baseUrl = "https://x-clone1.onrender.com"
+export const baseUrl = "https://x-clone-vy92.onrender.com"
